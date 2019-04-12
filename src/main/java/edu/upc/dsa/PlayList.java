160ce6f403ffd168d367db8601d0337726cc30c9
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-04-12 17:30:34 +0200

    added playList class

---
+++ src/main/java/edu/upc/dsa/PlayList.java
@@ -0,0 +1,59 @@
+package edu.upc.dsa;
+
+import java.util.LinkedList;
+
+public class PlayList {
+    //atributos
+    private String id;
+    private String nombre;
+    private LinkedList<Titulo> titulos;
+    private Usuario usuario;
+
+    //constructores
+    public PlayList (){
+        this.titulos = new LinkedList<>();
+    }
+
+    public PlayList(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.titulos = titulos;
+        this.usuario = usuario;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Titulo> getTitulos() {
+        return titulos;
+    }
+
+    public void setTitulos(LinkedList<Titulo> titulos) {
+        this.titulos = titulos;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    //metodos
+    public void añadirTitulo (Titulo titulo){
+        this.titulos.add(titulo);
+    }
+}
